d55f070ac7f4f639a7994077c3f1398674d355f9
Craig Kelly <craig.n.kelly@gmail.com>
2016-08-11 15:47:30 -0500

    test sorted record read/write

--- twitterFile_test.go
+++ twitterFile_test.go
@@ -35,3 +35,42 @@ func TestEmptyTwitterFile(t *testing.T) {
 	data.WriteTwitterFile(tmpfile.Name())
 	assert.Empty(ReadTwitterFile(tmpfile.Name()))
 }
+
+func TestSortingTwitterFileRecords(t *testing.T) {
+	assert := assert.New(t)
+
+	tmpfile, err := ioutil.TempFile("", "twivility")
+	pcheck(err)
+	defer os.Remove(tmpfile.Name())
+
+	makeOne := func(tid int64, uid int64, s string) TweetFileRecord {
+		return TweetFileRecord{
+			TweetID:   tid,
+			UserID:    uid,
+			UserName:  s,
+			Text:      s,
+			Timestamp: s,
+		}
+	}
+
+	input := TweetFileRecordSlice{
+		makeOne(1, 3, "C"),
+		makeOne(2, 2, "B"),
+		makeOne(3, 1, "A"),
+	}
+	input.WriteTwitterFile(tmpfile.Name())
+
+	output := ReadTwitterFile(tmpfile.Name())
+	assert.Equal(len(input), len(output))
+	seen := output.Seen()
+	assert.Contains(seen, int64(1))
+	assert.Contains(seen, int64(2))
+	assert.Contains(seen, int64(3))
+	assert.Equal(3, len(seen))
+
+	// MinMax only works if the sort worked - which leaves 2
+	mn, mx := output.MinMax()
+	assert.Equal(int64(1), mn)
+	assert.Equal(int64(2), output[1].TweetID)
+	assert.Equal(int64(3), mx)
+}
