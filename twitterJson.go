0839bec7689d31ed8ba1424275795d710f80a05c
Craig Kelly <craig.n.kelly@gmail.com>
2016-08-11 16:06:38 -0500

    Testing our twitter JSON stuff

--- twitterJson.go
+++ twitterJson.go
@@ -4,14 +4,14 @@ import (
 	"encoding/json"
 )
 
-// Provide JSON-export capability for records read from twitterFile
-type twitterJSON struct {
+// TwitterJSON provides JSON-export capability for records read from twitterFile
+type TwitterJSON struct {
 	TweetList TweetFileRecordSlice
 }
 
 // CreateTwitterJSON creates and returns a JSON string of the given records
 func CreateTwitterJSON(recs TweetFileRecordSlice) []byte {
-	topLevel := twitterJSON{
+	topLevel := TwitterJSON{
 		TweetList: recs,
 	}
 	txt, _ := json.Marshal(topLevel)
---
+++ twitterJson_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestSieve - simple unit testing for our prime factoring function
+func TestTwitterJSON(t *testing.T) {
+	assert := assert.New(t)
+
+	makeOne := func(tid int64, uid int64, s string) TweetFileRecord {
+		return TweetFileRecord{
+			TweetID:   tid,
+			UserID:    uid,
+			UserName:  s,
+			Text:      s,
+			Timestamp: s,
+		}
+	}
+
+	input := TweetFileRecordSlice{
+		makeOne(1, 3, "C"),
+		makeOne(2, 2, "B"),
+		makeOne(3, 1, "A"),
+	}
+
+	data := CreateTwitterJSON(input)
+
+	output := TwitterJSON{TweetList: make([]TweetFileRecord, 0)}
+	json.Unmarshal(data, &output)
+
+	assert.Equal(3, len(output.TweetList))
+
+	assert.Equal(input, output.TweetList)
+}
