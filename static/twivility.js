6548135a34babc6b8653e731d799b0fd8bf655ef
Craig Kelly <craig.n.kelly@gmail.com>
2016-09-19 15:42:57 -0500

    Proper coloring and sorting for our blocks

--- static/twivility.js
+++ static/twivility.js
@@ -1,8 +1,8 @@
 // routines specific to twivility: we assume jQuery and lodash are available
 
 // Helpful mixins for lodash
-_.mixin({
-    'prop': function(obj, propname) {
+(function() {
+    function prop(obj, propname) {
         if (_.isArray(obj) && _.isNumber(propname)) {
             return obj[propname];
         }
@@ -13,7 +13,60 @@ _.mixin({
             return obj[propname];
         }
     }
-});
+
+    function trim(s) {
+        if (!s && s !== 0 && s !== false)
+            return "";
+
+        var ss = "" + s;
+        if (!ss || !ss.length || ss.length < 1) {
+            return "";
+        }
+
+        if (ss.trim) {
+            return ss.trim();
+        }
+        else {
+            return ss.replace(/^\s+|\s+$/gm, '');
+        }
+    }
+
+    function matchAny(s, checks) {
+        var toCheck = trim(s);
+        if (s === "")
+            return false;
+        for (var i = 0; i < checks.length; ++i) {
+            if (toCheck.match(checks[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    var leftChecks = [
+        /clinton/i,
+        /kaine/i
+    ];
+    function isLefty(s) {
+        return matchAny(s, leftChecks);
+    }
+
+    var rightChecks = [
+        /trump/i,
+        /pence/i
+    ];
+    function isRighty(s) {
+        return matchAny(s, rightChecks);
+    }
+
+    _.mixin({
+        'prop': prop,
+        'trim': trim,
+        'isLefty': isLefty,
+        'isRighty': isRighty
+    });
+})();
+
 
 
 // Actual twivility work
@@ -34,13 +87,20 @@ _.mixin({
 
     // Get all accts, get a all tweets per acct, and then call acctCallback
     // with acct, tweetlist
-    function getAllAccts(acctCallback) {
+    function getAllAccts(acctCallback, finishedCallback) {
+        var count = 0;
+
         return $.get("/accts")
             .done(function(data) {
                 _.each(data, function(acct){
+                    count += 1;
                     getSingleAcct(acct, function(tweets){
                         if (!!acctCallback) {
                             acctCallback(acct, tweets);
+                        }
+                        count -= 1;
+                        if (count < 1 && !!finishedCallback) {
+                            finishedCallback();
                         }
                     });
                 });
