124ed808189d3aa25d48fcedfc2fa72caad097f2
Craig Kelly <craig.n.kelly@gmail.com>
2017-03-03 10:06:36 -0600

    Upgrade to go 1.8 and update deps

--- vendor/github.com/dghubble/go-twitter/twitter/search.go
+++ vendor/github.com/dghubble/go-twitter/twitter/search.go
@@ -2,7 +2,6 @@ package twitter
 
 import (
 	"net/http"
-	"net/url"
 
 	"github.com/dghubble/sling"
 )
@@ -53,9 +52,8 @@ type SearchTweetParams struct {
 }
 
 // Tweets returns a collection of Tweets matching a search query.
-// https://api.twitter.com/1.1/search/tweets.json
+// https://dev.twitter.com/rest/reference/get/search/tweets
 func (s *SearchService) Tweets(params *SearchTweetParams) (*Search, *http.Response, error) {
-	params.Query = url.QueryEscape(params.Query)
 	search := new(Search)
 	apiError := new(APIError)
 	resp, err := s.sling.New().Get("tweets.json").QueryStruct(params).Receive(search, apiError)
